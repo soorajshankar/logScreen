465cf2b5e723ef11db634d83134c7e21b90e96cb
soorajshankar <soorajshankar@gmail.com>
2024-02-13 19:06:30 +0530

    ++

--- index.js
+++ index.js
@@ -66,7 +66,109 @@ const html = `<!DOCTYPE html>
   <script src="https://unpkg.com/babel-standalone@6.26.0/babel.min.js"></script>
 
   <script type="text/babel">
-    const { useState, useEffect } = React;
+    const { useState, useEffect, useMemo } = React;
+    class ErrorBoundary extends React.Component {
+      constructor(props) {
+        super(props);
+        this.state = { hasError: false };
+      }
+    
+      static getDerivedStateFromError(error) {
+        return { hasError: true };
+      }
+    
+      componentDidCatch(error, errorInfo) {
+        console.error('Error caught by ErrorBoundary:', error, errorInfo);
+        console.log('props:', this.props);
+      }
+    
+      render() {
+        if (this.state.hasError) {
+          return (
+            <div className="p-4 bg-red-200 text-red-800">
+              <p>Something is not right in here.</p>
+            </div>
+          );
+        }
+    
+        return this.props.children;
+      }
+    }
+    
+
+    const LogViewer = ({ logs }) => {
+      const MAX_CHARACTERS = 500; // Maximum characters to display for each log message
+    
+      const [selectedLog, setSelectedLog] = useState(null);
+    
+      const truncateMessage = useMemo(() => {
+        return (message) => {
+          if (typeof message !== 'string') message = JSON.stringify(message, null, 2);
+          return message.length > MAX_CHARACTERS
+            ? message.slice(0, MAX_CHARACTERS) + '...'
+            : message;
+        };
+      }, [MAX_CHARACTERS]);
+    
+      const openFullLog = (log) => {
+        setSelectedLog(log);
+      };
+    
+      const closeFullLog = () => {
+        setSelectedLog(null);
+      };
+    
+      return (
+        <div className="flex flex-col h-screen p-4 bg-gray-100">
+          {logs.toReversed().map((log, index) => (
+            <ErrorBoundary key={index}>
+              <div
+                className="flex items-start border-b border-gray-300 py-2 hover:bg-blue-100 cursor-pointer transition-all"
+                onClick={() => openFullLog(log)}
+              >
+                <div className="w-1/4 pr-4">
+                  <span className="text-gray-500">{log.timestamp}</span>
+                </div>
+                <div className="w-3/4">
+                  <pre className="text-gray-700 whitespace-pre-wrap break-all">
+                    {truncateMessage(log.message)}
+                  </pre>
+                </div>
+              </div>
+            </ErrorBoundary>
+          ))}
+    
+          {selectedLog && (
+            <div className="fixed top-0 right-0 bottom-0 bg-white w-1/2 p-4 overflow-hidden shadow-lg">
+              <div className="flex justify-between items-center mb-4">
+                <h2 className="text-xl font-bold">Full Log</h2>
+                <div>
+                  <button className="text-blue-500" onClick={closeFullLog}>
+                    Close
+                  </button>
+                  <button
+                    className="bg-blue-500 text-white py-2 px-4 ml-2"
+                    onClick={() => {
+                      navigator.clipboard.writeText(JSON.stringify(selectedLog.message, null, 2)).then(function(x) {
+                        alert('Copied to clipboard!');
+                      })
+                    }}
+                  >
+                    Copy
+                  </button>
+                </div>
+              </div>
+              <div className="max-h-screen overflow-y-auto bg-gray-800 rounded p-4">
+                <pre className="text-white whitespace-pre-wrap break-all">
+                  {JSON.stringify(selectedLog.message, null, 2)}
+                </pre>
+              </div>
+            </div>
+          )}
+          
+        </div>
+      );
+    };
 
     function App() {
       const [logs, setLogs] = useState([]);
@@ -79,9 +181,9 @@ const html = `<!DOCTYPE html>
 
           try {
             const parsedJSON = JSON.parse(logline);
-            setLogs(prevLogs => [...prevLogs, { timestamp, content: parsedJSON }]);
+            setLogs(prevLogs => [...prevLogs, { timestamp, message: parsedJSON }]);
           } catch (error) {
-            setLogs(prevLogs => [...prevLogs, { timestamp, content: logline }]);
+            setLogs(prevLogs => [...prevLogs, { timestamp, message: logline }]);
           }
         }
 
@@ -95,22 +197,8 @@ const html = `<!DOCTYPE html>
 
       return (
         <div>
-          <h1 className="text-3xl mb-6">Real-time Input with React</h1>
-          <ul className="list-none p-0">
-            {logs.map((log, index) => (
-              <li key={index} className="bg-white shadow-md rounded-md p-4 mb-4">
-                <div className="text-sm text-gray-500">{log.timestamp}</div>
-                {typeof log.content === 'object' ? (
-                  <div className="json-container">
-                    <button onClick={() => alert('Expand button clicked')}>Expand</button>
-                    <pre>{JSON.stringify(log.content, null, 2)}</pre>
-                  </div>
-                ) : (
-                  <div className="text-lg">{log.content}</div>
-                )}
-              </li>
-            ))}
-          </ul>
+        <a className="text-3xl mb-6" href="https://github.com/soorajshankar/logScreen" target="_blank" rel="noopener noreferrer">| npx logscreen </a>
+        <LogViewer logs={logs} />
         </div>
       );
     }
